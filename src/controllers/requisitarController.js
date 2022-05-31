c8196a6dc2dc362d9bb070a29768f05db7700ee8
João Leka <joao.barbosa@sptech.school>
2022-05-31 12:08:28 -0300

    grafico funcionando

--- src/controllers/requisitarController.js
+++ src/controllers/requisitarController.js
@@ -3,20 +3,26 @@ var requisitarModel = require("../models/requisitarModel");
 var sessoes = [];
 
 function requisitar(req, res) {
-    requisitarModel.requisitar()
-        .then(function (resultado) {
-            if (resultado.length > 0) {
-                res.status(200).json(resultado);
-            } else {
-                res.status(204).send("Nenhum resultado encontrado!")
-            }
-        }).catch(
-            function (erro) {
-                console.log(erro);
-                console.log("Houve um erro ao realizar a consulta! Erro: ", erro.sqlMessage);
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
+    var idalbum = req.params.idalbum;
+  
+
+       
+    const limite_linhas = 7;
+
+console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
+
+    requisitarModel.requisitar(idalbum, limite_linhas).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+
 }
 
 
