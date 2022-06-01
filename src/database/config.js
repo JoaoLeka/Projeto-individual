31fcc09b78371340da4ef3e305af20cf5cbec236
João Leka <joao.barbosa@sptech.school>
2022-06-01 10:01:48 -0300

    cards funcionais

--- src/database/config.js
+++ src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "root",
+    user: "aluno",
     database: "twenty",
-    password: "123456789",
+    password: "sptech",
 };
 
 function executar(instrucao) {
